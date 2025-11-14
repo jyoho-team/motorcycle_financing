35d8b5ef183f5f839f5394306b5f5d6e2f444cb9
dtinoco <dtinoco@josys.com.mx>
2025-11-14 11:33:08 -0600

    first commit

---
+++ motorcycle_financing/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Motorcycle Financing',
+    'summary': 'Streamlines the loan application process for dealerships.',
+    'license': 'OPL-1',
+    'category': 'Kawiil/Custom Modules',
+    'author': 'Jyoho Systems',
+    'website': 'https://www.josys.com.mx',
+    'version': '0.0.1',
+    'depends': ['base'],
+    "application":True,
+    'data': [        
+    ],
+    'demo': [
+    ],
+} 
